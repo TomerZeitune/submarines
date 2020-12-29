7e038e51a503077c791ad2ca52449b65a0934e06
TomerZeitune <tomerzeitune@gmail.com>
2020-12-29 14:10:51 +0200

    Added messages constants

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,25 @@
-class MessageTypes:
+class MessageType:
     BEGIN_SESSION = 0
     YES = 1
-    NO = 0
+    NO = 2
+    CONTINUE_SESSION = 3
+    ATTACK_SUBMARINE = 4
+    ERROR = 5
+    DISCONNECT = 6
+    READY = 7
+    SUBMARINE_SINK = 8
+    KEEP_ALIVE = 9
 
 
-MESSAGE_DATA_LENGTH = {
-    MessageTypes.BEGIN_SESSION: 0
+MESSAGE_FIELDS_COUNTS = {
+    MessageType.BEGIN_SESSION: 3,
+    MessageType.YES: 0,
+    MessageType.NO: 0,
+    MessageType.CONTINUE_SESSION: 2,
+    MessageType.ATTACK_SUBMARINE: 2,
+    MessageType.ERROR: 1,
+    MessageType.DISCONNECT: 0,
+    MessageType.READY: 0,
+    MessageType.SUBMARINE_SINK: 3,
+    MessageType.KEEP_ALIVE: 0,
 }
