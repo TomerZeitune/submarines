9651c20019dfbcdb5880ba3ca8d7645481897879
TomerZeitune <tomerzeitune@gmail.com>
2020-12-29 15:26:22 +0200

    Moved deserialization to connection

--- src/message.py
+++ src/message.py
@@ -12,9 +12,3 @@ class Message:
             binary_data += pack("h", element)
         return pack("B", self.type_id) + binary_data
 
-    def deserialize(self, binary_data):
-        type_id = unpack("B", binary_data[0:1])[0]
-        data = []
-        for i in range(1, len(binary_data), 2):
-            data.append(unpack("h", binary_data[i:i+2])[0])
-        return Message(type_id, data)
