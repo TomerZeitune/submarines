bb9902b6014503f171fdce667df3464b99238c44
TomerZeitune <tomerzeitune@gmail.com>
2020-12-29 14:22:08 +0200

    Finished connection class (Untested)

--- src/connection.py
+++ src/connection.py
@@ -1,10 +1,19 @@
+from constants import MessageType, MESSAGE_FIELDS_COUNTS, FIELD_SIZE
+from message import Message
+import socket
+
+# TODO Test this code
+
+
 class Connection:
 
-    def __init__(self, socket):
-        self.socket = socket
+    def __init__(self, connection_socket):
+        self.sock = connection_socket
 
     def send(self, serializable):
-        self.socket.send(serializable.serialize())
+        self.sock.send(serializable.serialize())
 
     def receive(self):
-        type_id = self.socket.recv(1)[0]
+        type_id = self.sock.recv(1, socket.MSG_WAITALL)[0]
+        binary_data = self.sock.recv(MESSAGE_FIELDS_COUNTS[type_id] * FIELD_SIZE, socket.MSG_WAITALL)
+        return Message(type_id, binary_data)
